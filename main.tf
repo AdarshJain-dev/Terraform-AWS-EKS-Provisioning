15a49196fd88c941152e180e767f69a68ee9dc0c
Adarsh Kumar Jain <adarsh.jain428@gmail.com>
2026-02-01 16:27:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -227,6 +227,46 @@ resource "aws_eks_addon" "coredns" {
   addon_name        = "coredns"
   resolve_conflicts_on_create = "OVERWRITE"
 
+resource "aws_iam_role" "ebs_csi_irsa_role" {
+  name = "devopsshack-ebs-csi-irsa-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(data.aws_eks_cluster.devopsshack.identity[0].oidc[0].issuer, "https://", "")}:sub" =
+            "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
+  role       = aws_iam_role.ebs_csi_irsa_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name             = aws_eks_cluster.devopsshack.name
+  addon_name               = "aws-ebs-csi-driver"
+  resolve_conflicts_on_create = "OVERWRITE"
+  service_account_role_arn = aws_iam_role.ebs_csi_irsa_role.arn
+
+  depends_on = [
+    aws_eks_node_group.devopsshack
+  ]
+}
+
+
   depends_on = [
     aws_eks_node_group.devopsshack
   ]
